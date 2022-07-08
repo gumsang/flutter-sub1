23da7937f06217208fac4c56fd7561db4a35ee38
eunsang.kim <gomuzom@naver.com>
2022-07-08 19:26:35 +0900

    컴포넌트 작성, 예제리스트  생성

--- lib/youtube_detail_screen.dart
+++ lib/youtube_detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_clone/components/youtube_list.dart';
 
 class YoutubeDetailScreen extends StatelessWidget {
   const YoutubeDetailScreen(this.myPly, {Key? key}) : super(key: key);
--- lib/youtube_main_screen.dart
+++ lib/youtube_main_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sub_column_row_image/components/youtube_list.dart';
 
 class YoutubeMainScreen extends StatelessWidget {
-  const YoutubeMainScreen({Key? key}) : super(key: key);
+  YoutubeMainScreen({Key? key}) : super(key: key);
+
+//PlayList(this.title, this.date, this.id, this.idUrl, this.imageUrl)
+  final playList = List<PlayList>.generate(
+    20,
+    (i) => PlayList(
+        '[#티전드] (2시간) 수근이 김장 잘하지~^^ 개그 장인 이수근의 인간문화재 등극이 시급합니다🤣 | #신서유기8 #신서유기7',
+        '조회수 1,949,215회 · 2022. 5. 15.',
+        'tvN D ENT',
+        'https://yt3.ggpht.com/aQq9OjYxeYTQ9F3sgkmfmv0gH1bigSr5tJLyDR11RJFa9bGxgci5Jw0Y9tDP7eNBm8AQqyx1Qi8=s176-c-k-c0x00ffffff-no-rj',
+        'https://i.ytimg.com/vi/Q_TAgPgfoPk/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA2ePlCc5TzkDTTkQ0JAwLlEwRGdg'),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        // centerTitle: true,
+        title: Text("YouTube"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.tv)),
+          IconButton(
+              onPressed: () {}, icon: Icon(Icons.notifications_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.circle)),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return Container();
+        },
+      ),
+    );
   }
 }
